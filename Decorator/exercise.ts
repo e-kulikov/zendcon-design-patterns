52ed07844674208b1028dc7da783381e44108667
Yury-Soika <puma10009@gmail.com>
2023-02-11 14:52:35 +0100

    Decorator
    Solution by Yury Soika

--- Decorator/exercise.ts
+++ Decorator/exercise.ts
@@ -1,65 +1,78 @@
 interface HtmlElement {
-    __toString(): string;
-    getName(): string;
+  __toString(): string;
+  getName(): string;
 }
 
 class InputText implements HtmlElement {
-    protected name;
-    constructor(name: string) {
-        this.name = name;
-    }
-    getName() {
-        return this.name;
-    }
-    __toString() {
-        return `<input type="text" id="${this.name}" name="${this.name}" />`;
-    }
+  protected name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  getName() {
+    return this.name;
+  }
+  __toString() {
+    return `<input type="text" id="${this.name}" name="${this.name}" />`;
+  }
 }
 
 abstract class HtmlDecorator implements HtmlElement {
-    protected element;
+  protected element: HtmlElement;
 
-    // @todo here the code to implement
+  constructor(element: HtmlElement) {
+    this.element = element;
+  }
+
+  getName(): string {
+    return this.element.getName();
+  }
+
+  __toString(): string {
+    return this.element.__toString();
+  }
 }
 
 class LabelDecorator extends HtmlDecorator {
-    protected label;
-    setLabel(label) {
-        this.label = label;
-    }
-    __toString() {
-        const name = this.getName();
-        return `
-            <label for="${name}">${this.label}</label>
-            ${this.element.__toString()}
-        `;
-    }
+  protected label: any;
+  setLabel(label: string) {
+    this.label = label;
+  }
+  __toString() {
+    const name = this.getName();
+    return `
+              <label for="${name}">${this.label}</label>
+              ${this.element.__toString()}
+          `;
+  }
 }
 
 class ErrorDecorator extends HtmlDecorator {
-    protected error;
-    setError(message) {
-        this.error = message;
-    }
-    __toString() {
-        return `
-            ${this.element.__toString()}<span>${this.error}</span>
-        `;
-    }
+  protected error: any;
+  setError(message: string) {
+    this.error = message;
+  }
+  __toString() {
+    return `
+              ${this.element.__toString()}
+              <span>${this.error}</span>
+          `;
+  }
 }
 
-const input = new InputText('nickname');
-console.log(`InputText without decorator: ${input}`);
+const input = new InputText("nickname");
+console.log(`InputText without decorator: ${input.__toString()}`);
 
 const labelled = new LabelDecorator(input);
-labelled.setLabel('Nickname:');
-console.log(`InputText with LabelDecorator:${labelled}`);
+labelled.setLabel("Nickname:");
+console.log(`InputText with LabelDecorator:${labelled.__toString()}`);
 
 const error = new ErrorDecorator(input);
-error.setError('You must enter a unique nickname');
-console.log(`InputText with ErrorDecorator:${error}`);
+error.setError("You must enter a unique nickname");
+console.log(`InputText with ErrorDecorator:${error.__toString()}`);
 
 // Label + Error
 const labelledError = new ErrorDecorator(labelled);
-labelledError.setError('You must enter a unique nickname');
-console.log(`InputText with LabelDecorator and ErrorDecorator:${labelledError}`);
+labelledError.setError("You must enter a unique nickname");
+console.log(
+  `InputText with LabelDecorator and ErrorDecorator:${labelledError.__toString()}`
+);
