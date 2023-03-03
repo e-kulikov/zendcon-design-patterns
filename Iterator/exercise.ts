d876fb6f3ae32f9ddbc48abfbb8a69c9c66fb9e2
Yury-Soika <puma10009@gmail.com>
2023-03-03 12:28:12 +0100

    Iterator
    Iterator solution by Yury Soika

--- Iterator/exercise.ts
+++ Iterator/exercise.ts
@@ -1,3 +1,24 @@
 export class Fibonacci implements IterableIterator<number> {
-    // @todo: here the code to implement
+  protected lenght: number;
+  protected a: number;
+  protected b: number;
+  constructor(length: number) {
+    this.lenght = length;
+    this.a = 0;
+    this.b = 1;
+  }
+  [Symbol.iterator](): IterableIterator<number> {
+    return this;
+  }
+  next(): IteratorResult<number, any> {
+    if (this.lenght > 0) {
+      const result = { value: this.a, done: false };
+      this.lenght--;
+      const temp = this.b;
+      this.b = this.a + this.b;
+      this.a = temp;
+      return result;
+    }
+    return { value: this.a, done: true };
+  }
 }
