5e3be0c45357c4e2733afc0e3f76f790778b8e0c
ekulikov <evgeny.kulikov@outlook.com>
2023-03-20 12:29:07 +0100

    Iterator solution

--- Iterator/exercise.ts
+++ Iterator/exercise.ts
@@ -3,18 +3,16 @@ export class Fibonacci implements Iterable<number> {
 
     [Symbol.iterator](): Iterator<number> {
       let current = 0;
-      let next = 1;
+      let prev = 0;
       let index = 0;
 
       return {
         next: (): IteratorResult<number> => {
-          if (index < this.length) {
-            const value = current;
-            [current, next] = [next, current + next];
-            index++;
-            return { value, done: false };
+          if (index++ < this.length) {
+            [prev, current] = [current, (prev + current) || 1];
+            return { value: prev, done: false };
           } else {
-            return { value: null as any, done: true };
+            return { value: undefined, done: true };
           }
         }
       };
