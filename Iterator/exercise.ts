b18e7dcd3cb6281691a08823b9dd4a31e52bd0d3
ekulikov <evgeny.kulikov@outlook.com>
2023-03-21 14:28:27 +0100

    TS rewrite
    Iterator solution in more OO way

--- Iterator/exercise.ts
+++ Iterator/exercise.ts
@@ -1,21 +1,19 @@
 export class Fibonacci implements Iterable<number> {
+    private current: number = 0;
+    private prev: number = 0;
+    private index: number = 0;
     constructor(private length: number) {}
 
-    [Symbol.iterator](): Iterator<number> {
-      let current = 0;
-      let prev = 0;
-      let index = 0;
-
-      return {
-        next: (): IteratorResult<number> => {
-          if (index++ < this.length) {
-            [prev, current] = [current, (prev + current) || 1];
-            return { value: prev, done: false };
-          } else {
+    next(): IteratorResult<number> {
+        if (this.index++ < this.length) {
+            [this.prev, this.current] = [this.current, (this.prev + this.current) || 1];
+            return { value: this.prev, done: false };
+        } else {
             return { value: undefined, done: true };
-          }
         }
-      };
     }
-  }
 
+    [Symbol.iterator](): Iterator<number> {
+        return this;
+    }
+}
