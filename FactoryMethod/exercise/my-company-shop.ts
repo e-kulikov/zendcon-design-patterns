25d9efd5cf8accb99d90b7342761c815ac1373ad
Yury-Soika <puma10009@gmail.com>
2023-04-13 19:07:25 +0200

    Factory method

--- FactoryMethod/exercise/my-company-shop.ts
+++ FactoryMethod/exercise/my-company-shop.ts
@@ -1,12 +1,35 @@
-import { ProductCreator, ProductInterface } from './shopping-cart-framework'
+import { ProductCreator, ProductInterface } from "./shopping-cart-framework";
 export class ProductCatalogA extends ProductCreator {
-    // todo: here the code to implement
+  private catalog: ProductCatalogDB;
+  public constructor(catalog: ProductCatalogDB) {
+    super();
+    this.catalog = catalog;
+  }
+  public createProduct(code: string): ProductInterface {
+    return new MyShopProduct(code, this.catalog[code]);
+  }
+
+  getMarketingDescription(code: string): string {
+    const product = this.createProduct(code);
+    return `${product.getShopProductCode()} - ${product.getShopDescription()}`;
+  }
 }
 
 interface ProductCatalogDB {
-    [code: string]: string
+  [code: string]: string;
 }
 
 export class MyShopProduct implements ProductInterface {
-    // todo: here the code to implement
+  private code: string;
+  private description: string;
+  public constructor(code: string, description: string) {
+    this.code = code;
+    this.description = description;
+  }
+  getShopProductCode(): string {
+    return this.code;
+  }
+  getShopDescription(): string {
+    return this.description;
+  }
 }
