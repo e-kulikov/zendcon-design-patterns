de0b7fc6d915ce29ecd68e3d692eedb8a57388d1
Yury-Soika <puma10009@gmail.com>
2023-04-13 19:07:52 +0200

    Update other-company-shop.ts

--- FactoryMethod/exercise/other-company-shop.ts
+++ FactoryMethod/exercise/other-company-shop.ts
@@ -1,12 +1,38 @@
-import { ProductCreator, ProductInterface } from './shopping-cart-framework'
+import { ProductCreator, ProductInterface } from "./shopping-cart-framework";
 export class ProductCatalogB extends ProductCreator {
-    // todo: here the code to implement
+  private catalog: ProductCatalogBDB;
+  public constructor(catalog: ProductCatalogBDB) {
+    super();
+    this.catalog = catalog;
+  }
+  public createProduct(code: string): ProductInterface {
+    return new MyShopProduct(code, this.catalog[code]);
+  }
+
+  getMarketingDescription(code: string): string {
+    const product = this.createProduct(code);
+    return `${product.getShopProductModel()} ${product.getShopProductCode()}: ${product.getShopDescription()}`;
+  }
 }
 
 export interface ProductCatalogBDB {
-    [code: string]: [string, string]
+  [code: string]: [string, string];
 }
 
 export class MyShopProduct implements ProductInterface {
-    // todo: here the code to implement
+  private code: string;
+  private description: [string, string];
+  public constructor(code: string, description: [string, string]) {
+    this.code = code;
+    this.description = description;
+  }
+  getShopProductCode(): string {
+    return this.code;
+  }
+  getShopDescription(): string {
+    return this.description[0];
+  }
+  getShopProductModel(): string {
+    return this.description[1];
+  }
 }
